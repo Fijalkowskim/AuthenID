63bc87776bc67ba33c40e54564d19c1c9b645bcb
Mateusz <fijalkowskim01@gmail.com>
2025-10-02 15:17:36 +0200

    Backend initialization

---
+++ backend/src/main/java/com/fijalkowskim/AuthenID/AuthenIdApplication.java
@@ -0,0 +1,13 @@
+package com.fijalkowskim.AuthenID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenIdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fijalkowskim/AuthenID/AuthenIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fijalkowskim.AuthenID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
